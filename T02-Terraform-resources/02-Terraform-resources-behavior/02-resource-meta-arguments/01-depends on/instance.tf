b08325d3dddb75e04501e62befe230f10247e5fb
raghunath0326 <raghunath0326@gmail.com>
2022-07-13 17:11:19 +0530

    security rules seperated

--- T02-Terraform-resources/02-Terraform-resources-behavior/02-resource-meta-arguments/01-depends on/instance.tf
+++ T02-Terraform-resources/02-Terraform-resources-behavior/02-resource-meta-arguments/01-depends on/instance.tf
@@ -1,10 +1,12 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0c2b8ca1dad447f8a"
+  ami           = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
   key_name = "terraform-demo"
   subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.test-security-group.id]
   user_data = file("nginx-install.sh")
+  availability_zone = "us-east-1a"
+  associate_public_ip_address = false
 
   tags = {
     "Name" = "WebApp-terraform"
